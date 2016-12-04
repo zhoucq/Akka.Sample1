79ba75269dcd876dea14e9f6accf56f7dacc9a49
Zhou ChengQun <zcq0328@gmail.com>
2016-12-04 14:56:28 +0800

    hello world sample

--- Akka.HelloWorld/Program.cs
+++ Akka.HelloWorld/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Akka.Actor;
 
 namespace Akka.HelloWorld
 {
@@ -10,6 +11,27 @@ namespace Akka.HelloWorld
     {
         static void Main(string[] args)
         {
+            // 生成一个actor system
+            var actorSystem = ActorSystem.Create("HelloSystem");
+
+
+            Props props = Props.Create<HelloActor>();
+            IActorRef helloActor = actorSystem.ActorOf(props, "helloActor");
+
+            helloActor.Tell("World");
+
+            
+            Console.ReadLine();
         }
     }
+
+    public class HelloActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            Console.WriteLine("Hello " + message);
+        }
+    }
+
+   
 }
